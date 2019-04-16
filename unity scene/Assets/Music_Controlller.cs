050c9c33de96c158117b003b87df91237f1b98a1
mzx0116 <335494426@qq.com>
2019-04-16 10:22:13 +0100

    first version terrain

---
+++ unity scene/Assets/Music_Controlller.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Controlller : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        AkSoundEngine.RegisterGameObj(gameObject);
+        AkSoundEngine.SetSwitch("Exploration_State", "On_Foot", gameObject);
+        StartMusic();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartMusic()
+    {
+        AkSoundEngine.PostEvent("Play_Music", gameObject);
+    }
+    
+    void StopMusic()
+    {
+        AkSoundEngine.PostEvent("Stop_Music", gameObject);
+    }
+}
---
+++ unity scene/Assets/Music_State_Switch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_State_Switch : MonoBehaviour {
+
+    public bool DebugInfo;
+
+    public string EnterState = "default";
+    public string ExitState = "default";
+    public GameObject mPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {        
+        AkSoundEngine.SetSwitch("Exploration_State", EnterState, mPlayer);
+        if (DebugInfo == true) { print("Music_Switch"); }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        AkSoundEngine.SetSwitch("Exploration_State", ExitState, mPlayer);
+        if (DebugInfo == true) { print("Music_Switch"); }
+    }
+}
